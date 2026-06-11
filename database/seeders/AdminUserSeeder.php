c6d9c5eef187897d3761c5f3ac7afe449e88c706
pooja14752282 <pooja.bm@marutsoft.com>
2026-06-11 12:09:01 +0530

    login changes

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        DB::table('users')->insertOrIgnore([
+            'email'      => 'admin@research-internship.com',
+            'password'   => Hash::make('123456'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
